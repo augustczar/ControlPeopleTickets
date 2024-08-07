428b2176c171625985b06dde868d1188af0cb4e9
augustczar <augustczarramos@gmail.com>
2024-08-07 09:49:54 -0300

    Initial creation of PersonService and TicketService projects

---
+++ Ticketservice/src/test/java/com/augustczar/controlpeopletickets/TicketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustczar.controlpeopletickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
